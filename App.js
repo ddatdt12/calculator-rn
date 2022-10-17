de7ce49fcea66d4fd26487dfef9bd11c67f44d25
Đỗ Đạt <ddatdt12@gmail.com>
2022-10-17 21:26:57 +0700

    init basic calculator

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const textInputRef = useRef(null);
+  const [text, setText] = useState("");
+
+  const handleButtonPress = () => {
+    try {
+      if (!textInputRef?.current?.value) {
+        setText("Please enter an expression!");
+        return;
+      }
+      let result = eval(textInputRef.current.value);
+      setText("Result: " + result);
+      textInputRef.current.style = { ...styles.textInput };
+    } catch (error) {
+      setText("Invalid input");
+      textInputRef.current.style.borderColor = "red";
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 50 }}>Calculator!</Text>
+      <TextInput ref={textInputRef} style={styles.textInput} />
+      <Button title="Calculate" onPress={handleButtonPress} />
+      {text && <Text style={{ fontSize: 30, marginTop: 20 }}>{text}</Text>}
     </View>
   );
 }
@@ -13,8 +33,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 40,
+  },
+  error: {
+    color: "red",
+    marginBottom: 10,
+    fontSize: 20,
+  },
+  textInput: {
+    borderColor: "blue",
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10,
+    fontSize: 30,
+    marginBottom: 20,
   },
 });
